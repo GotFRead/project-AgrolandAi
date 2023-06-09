118d9a81f4200da1c749f2301d85f6ad420d4a11
TrishinA <trishin.aoff@mail.ru>
2023-06-09 12:39:46 +0300

    feat: Перенос в helpers

--- app/helpers/converte_image.py
+++ app/helpers/converte_image.py
@@ -1,16 +1,10 @@
 
-import os
 import io
 import PIL.Image as Image
-from array import array
+
 
 def read_image_from_bytearray(bytearray):
-    return Image.fromarray(bytearray)
-
-# bytes = read_image_from_bytearray(path)
-# image = Image.open(io.BytesIO(bytes))
-# image.save()
-
+    return Image.open(io.BytesIO(bytearray))
 
 if __name__ == '__main__': 
     read_image_from_bytearray()
